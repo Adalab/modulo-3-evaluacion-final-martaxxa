7bb45f036f1899e17d505e0db7cc158fce8c86dc
martaxxa <mchacartegui29@gmail.com>
2025-02-05 16:37:54 +0100

    Add header and styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,13 @@
 import '../styles/App.scss';
 
+import dataJson from '../data/data.json';
+
 function App() {
   return (
-    <div>
-      Escribe aquí tu HTML...
-    </div>
+    <header className='header'>
+      <img className='header__img' src='src/images/WOW.jpeg' alt='Owen Wilson meme'/>
+      <h1 className='header__title'>Owen Wilson's "wow"</h1>
+    </header>
   )
 }
 
